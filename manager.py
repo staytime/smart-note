b786e2939fcb7bb22769e5835f38be81b29a22fb
徐志逢 <staytimeby1964@gmail.com>
2017-05-26 00:59:01 +0800

    add new feature "push" for adding new note file add config file (WIP)

--- manager.py
+++ manager.py
@@ -8,6 +8,45 @@ import os
 import os.path
 import logging
 import argparse
+import configparser
+
+
+
+class BufferStack(object):
+	def __init__(self, file):
+		self.stack = list()
+		self.file = file
+
+		if os.path.exists(file):
+			with open(file, 'rt') as f:
+				__ = f.read()
+				__ = [x.strip() for x in __.split('\n')]
+				__ = [x for x in __ if len(x) > 0]
+				for i in __:
+					self.push(i)
+
+	def save(self):
+		with open(self.file, 'wt') as f:
+			__ = '\n'.join(self.stack)
+			f.write(__)
+
+	def last(self):
+		return self.stack[-1]
+
+	def push(self, x):
+		self.stack.append(x)
+
+	def pop(self):
+		return self.pop()
+
+def getNewName():
+	import time
+	from hashlib import sha1
+
+	h = sha1()
+	h.update(b'0123456')
+	h.update(str(time.time()).encode('utf-8'))
+	return h.hexdigest()
 
 
 
@@ -15,16 +54,36 @@ import argparse
 __LOCATION__ = os.path.dirname(__file__)
 
 
+
+# __CONFIG__ = os.path.join(__LOCATION__, 'config.ini')
+
+# config = configparser.ConfigParser()
+# config.read(__CONFIG__)
+# config.sections()
+
+
+
+
+
 # configuration
 __EDITOR__               = 'C:/Program Files/Notepad++/notepad++.exe'
 __OPTION__               = '-n9999999'
 
 __DEFAULT_NOTE_FILE__    = 'note.txt'
-__DEFAULT_NOTE_MESSAGE__ = '\n\n\nNEW NOTE'
+__DEFAULT_NOTE_MESSAGE__ = '\n\n\nNEW NOTE\n\n\n'
 
-__LOG_FILE__ = 'note.log'
-__LOG_FILE__ = os.path.join(__LOCATION__, __LOG_FILE__)
+__STACK__                = os.path.join(__LOCATION__, 'stack.txt')
 
+__STACK__                = BufferStack(__STACK__)
+
+
+
+
+
+# get abs path
+__DEFAULT_NOTE_FILE__ = os.path.join(__LOCATION__, __DEFAULT_NOTE_FILE__)
+
+__LOG_FILE__ = os.path.join(__LOCATION__, 'note.log')
 
 
 
@@ -35,7 +94,8 @@ logging.basicConfig(
 logging.info('start process @"%s"' % __LOCATION__)
 logging.info('begin setup')
 
-__DEFAULT_NOTE_FILE__ = os.path.join(__LOCATION__, __DEFAULT_NOTE_FILE__)
+
+
 
 # initialize command pipe line
 __COMMAND__ = list()
@@ -59,7 +119,7 @@ logging.info('goto main')
 if __name__ == '__main__':
 
 	# the white list of commands
-	__CMD__ = ['new', 'log', 'open']
+	__CMD__ = ['new', 'log', 'open', 'push']
 	
 
 
@@ -92,6 +152,14 @@ if __name__ == '__main__':
 		elif command == 'log':
 			__COMMAND__.append(__LOG_FILE__)
 
+		elif command == 'push':
+			__ = getNewName()
+			__STACK__.push(__)
+			__ = os.path.join(__LOCATION__, __)
+			__COMMAND__.append(__)
+			create(__, __DEFAULT_NOTE_MESSAGE__)
+			
+
 		else:
 			raise Exception('function "%s" havn\'t be implement yet.' % command)
 
@@ -108,6 +176,8 @@ if __name__ == '__main__':
 		logging.error(str(e))
 		exit(-1)
 
+
+	__STACK__.save()
 
 
 
